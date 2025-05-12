b3d86a130c37d7ae2b04616db0527816ab13675f
Ranjith01111 <167077903+Ranjith01111@users.noreply.github.com>
2025-05-12 10:45:44 +0530

    Time: 0 ms (100%), Space: 8.1 MB (47.54%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,21 @@
+bool isPalindrome(int x) {
+   
+   int temp = x;
+   long rev = 0;
+
+        while(temp > 0){
+            int d = temp % 10;
+            rev = rev * 10 + d;
+
+            temp /= 10;
+        }
+
+        if(rev == x){
+            return true;
+        }else{
+            return false;
+        }
+
+         
+    
+}
