aca53421334631c23ad5659984ba3753f85db638
Ranjith01111 <167077903+Ranjith01111@users.noreply.github.com>
2025-05-12 16:04:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (98.11%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+         return ranjith(num,0);
+    }
+    
+int ranjith(int num , int steps){
+        if(num == 0){
+            return steps; 
+        }
+
+        if(num %2 ==0){
+            return ranjith(num/2,steps+1);
+        }else{
+            return ranjith(num-1,steps+1);
+        }
+
+}
+
+
+ 
+
+};
