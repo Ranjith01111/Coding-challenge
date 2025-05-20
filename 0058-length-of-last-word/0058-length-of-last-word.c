04d3d63ae1f4a6d2d88a5f64e3181a3469cf3986
Ranjith01111 <167077903+Ranjith01111@users.noreply.github.com>
2025-05-20 16:17:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (75.71%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,14 @@
+int lengthOfLastWord(char* s) {
+    int count = 0;
+    for(int i=strlen(s)-1;i>=0;i--){
+        if(s[i] != ' '){
+            count++;
+        }
+        else{
+            if(count > 0){
+                return count;
+            }
+        }
+    }
+    return count;
+}
